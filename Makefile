d65203500a4e998ecfc14835222d2dff67744c75
mlavin <markdlavin@gmail.com>
2012-12-14 20:39:26 -0500

    Load static resources with RequireJS and step to build optimized version.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 STATIC_DIR = ./sickmuse/static
 
 
-install:
+install-static:
 	# Install Bower components
 	# Requires bower
 	cd ${STATIC_DIR} && bower install
@@ -14,3 +14,9 @@ build-css:
 	mkdir -p ${STATIC_DIR}/fonts/
 	cp ${STATIC_DIR}/libs/font-awesome/font/* ${STATIC_DIR}/font/
 	lessc -x ${STATIC_DIR}/less/sickmuse.less ${STATIC_DIR}/css/sickmuse.css
+
+
+build-js:
+	# Build optimized JS
+	# Requires r.js
+	cd ${STATIC_DIR}/js && r.js -o name=sickmuse out=sickmuse-built.js baseUrl=. mainConfigFile=sickmuse.js
--- sickmuse/static/js/sickmuse.js
+++ sickmuse/static/js/sickmuse.js
@@ -1,83 +1,106 @@
-(function ($){
-    $('.plugin-graph').each(function (i, elem) {
-        var url = $(elem).data('url');
-        var series = [];
-        var id = $(elem).attr('id');
-        $.getJSON(url, function (data) {
-            // Fetch data and build the graph
-            series = [];
-            // Underscore would help here
-            $.each(data, function (key, metric) {
-                var interval = (metric.end - metric.start) / metric.timeline.length;
-                var timeline = metric.timeline.map(function (value, index) {
-                    var x = (metric.start + index * interval) * 1000,
-                        y = value;
-                    return [x, y];
+var jQueryPluginShim = {exports: 'jQuery', deps: ['jquery']};
+var flotPluginShim = {exports: 'jQuery', deps: ['flot']};
+
+require.config({
+    paths: {
+        jquery: '../libs/jquery/jquery.min',
+        flot: '../libs/flot/jquery.flot',
+        flotstack: '../libs/flot/jquery.flot.stack',
+        flottime: '../libs/flot/jquery.flot.time',
+        flotresize: '../libs/flot/jquery.flot.resize',
+        scrollspy: '../libs/bootstrap/js/bootstrap-scrollspy'
+    },
+    shim: {
+        flot: jQueryPluginShim,
+        flotstack: flotPluginShim,
+        flottime: flotPluginShim,
+        flotresize: flotPluginShim,
+        scrollspy: jQueryPluginShim
+    }
+});
+
+require(['jquery', 'flot', 'flotstack', 'flottime', 'flotresize', 'scrollspy'], function ($) {
+    (function ($){
+        $('.plugin-graph').each(function (i, elem) {
+            var url = $(elem).data('url');
+            var series = [];
+            var id = $(elem).attr('id');
+            $.getJSON(url, function (data) {
+                // Fetch data and build the graph
+                series = [];
+                // Underscore would help here
+                $.each(data, function (key, metric) {
+                    var interval = (metric.end - metric.start) / metric.timeline.length;
+                    var timeline = metric.timeline.map(function (value, index) {
+                        var x = (metric.start + index * interval) * 1000,
+                            y = value;
+                        return [x, y];
+                    });
+                    series.push({
+                        label: key,
+                        data: timeline
+                    });
                 });
-                series.push({
-                    label: key,
-                    data: timeline
-                });
-            });
-            
-            var plot = $.plot($(elem), series, {
-                colors: ["#DE5090", "#84C7E2", "#F7BECA", '#F2355B', '#FFDAC9', "#D4EDF4" ],
-                series: {
-                    stack: true,
-                    lines: {
-                        show: true,
-                        fill: true,
-                        steps: false,
-                        lineWidth: 1
+                
+                var plot = $.plot($(elem), series, {
+                    colors: ["#DE5090", "#84C7E2", "#F7BECA", '#F2355B', '#FFDAC9', "#D4EDF4" ],
+                    series: {
+                        stack: true,
+                        lines: {
+                            show: true,
+                            fill: true,
+                            steps: false,
+                            lineWidth: 1
+                        },
+                        shadowSize: 1
                     },
-                    shadowSize: 1
-                },
-                xaxis: {mode: "time", timezone: "browser"},
-                yaxis: {tickDecimals: 3},
-                grid: {hoverable: true, clickable: true},
-                legend: {
-                    container: $('#' + id + '-legend'),
-                    labelFormatter: function(label, series) {
-                        // Underscore or Handlebars would help here
-                        return '<a class="badge">' + label + '</a>';
+                    xaxis: {mode: "time", timezone: "browser"},
+                    yaxis: {tickDecimals: 3},
+                    grid: {hoverable: true, clickable: true},
+                    legend: {
+                        container: $('#' + id + '-legend'),
+                        labelFormatter: function(label, series) {
+                            // Underscore or Handlebars would help here
+                            return '<a class="badge">' + label + '</a>';
+                        }
                     }
-                }
-            });
-            // Store the plot and the original data set
-            $(elem).data('series', series);            
-            $(elem).data('plot', plot);
+                });
+                // Store the plot and the original data set
+                $(elem).data('series', series);            
+                $(elem).data('plot', plot);
 
-            var previousPoint = null;
-            $(elem).bind("plothover", function (event, pos, item) {
-                if (item) {
-                    if (previousPoint != item.dataIndex) {
-                        previousPoint = item.dataIndex;
-                        
+                var previousPoint = null;
+                $(elem).bind("plothover", function (event, pos, item) {
+                    if (item) {
+                        if (previousPoint != item.dataIndex) {
+                            previousPoint = item.dataIndex;
+                            
+                            $("#tooltip").remove();
+                            var x = item.datapoint[0].toFixed(2),
+                                y = item.datapoint[1].toFixed(2);
+
+                            // Underscore or Handlebars would help here
+                            $('<div>', {id: 'tooltip'})
+                                .addClass("tooltip tip fade in")
+                                .css({
+                                    display: 'block',
+                                    top: item.pageY + 5,
+                                    left: item.pageX + 5,
+                                }).append(
+                                    $('<div>').addClass('tooltip-arrow')
+                                ).append(
+                                    $('<div>').addClass('tooltip-inner')
+                                        .text(item.series.label + " " + y)
+                                ).appendTo("body");
+                        }
+                    }
+                    else {
                         $("#tooltip").remove();
-                        var x = item.datapoint[0].toFixed(2),
-                            y = item.datapoint[1].toFixed(2);
-
-                        // Underscore or Handlebars would help here
-                        $('<div>', {id: 'tooltip'})
-                            .addClass("tooltip tip fade in")
-                            .css({
-                                display: 'block',
-                                top: item.pageY + 5,
-                                left: item.pageX + 5,
-                            }).append(
-                                $('<div>').addClass('tooltip-arrow')
-                            ).append(
-                                $('<div>').addClass('tooltip-inner')
-                                    .text(item.series.label + " " + y)
-                            ).appendTo("body");
+                        previousPoint = null;            
                     }
-                }
-                else {
-                    $("#tooltip").remove();
-                    previousPoint = null;            
-                }
+                });
+                
             });
-            
         });
-    });
-})(jQuery);
+    })(jQuery);
+});
