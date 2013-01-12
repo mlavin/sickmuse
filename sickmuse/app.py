0b74f80766832ac6bf48506617f252900ec14c6a
mlavin <markdlavin@gmail.com>
2013-01-11 21:10:18 -0500

    Add graceful shutdown on SIGTERM and fast shutdown on SIGINT.

--- sickmuse/app.py
+++ sickmuse/app.py
@@ -1,7 +1,13 @@
 import glob
+import logging
 import os
+import signal
+import sys
+import time
 
-from tornado.ioloop import IOLoop
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado.process import task_id
 from tornado.options import define, parse_command_line, options
 from tornado.web import Application, url
 
@@ -47,15 +53,63 @@ class APIApplication(Application):
             self.plugin_info[host] = {'plugins': plugins}
 
 
+def shutdown(server, graceful=True):
+    """Shut down the application.
+
+    If a graceful stop is requested, waits for all of the IO loop's
+    handlers to finish before shutting down the rest of the process.
+    We impose a 10 second timeout.
+
+    Based on http://tornadogists.org/3428652/
+    """
+    ioloop = IOLoop.instance()
+
+    logging.info("Stopping server...")
+    # Stop listening for new connections
+    server.stop()
+
+    def final_stop():
+        ioloop.stop()
+        logging.info("Stopped.")
+        sys.exit(0)
+
+    def poll_stop(counts={'remaining': None, 'previous': None}):
+        remaining = len(ioloop._handlers)
+        counts['remaining'], counts['previous'] = remaining, counts['remaining']
+        previous = counts['previous']
+        # Wait until we only have only one IO handler remaining.  That
+        # final handler will be our PeriodicCallback polling task.
+        if remaining == 1:
+            final_stop()
+        if previous is None or remaining != previous:
+            logging.info("Waiting on IO %d remaining handlers", remaining)
+
+    if graceful:
+        # Callback to check on remaining handlers.
+        poller = PeriodicCallback(poll_stop, 250, io_loop=ioloop)
+        poller.start()
+
+        # Give up after 10 seconds of waiting.
+        ioloop.add_timeout(time.time() + 10, final_stop)
+    else:
+        final_stop()
+
+
 def main():
     # Setup and parse options
     parse_command_line()
-    # Start application
+    # Create application
     application = APIApplication(debug=options.debug, prefix=options.prefix)
-    application.listen(options.port, xheaders=True)
+    # Create server
+    server = HTTPServer(application, xheaders=True)
+    # Attach signal handlers
+    signal.signal(signal.SIGTERM, lambda sig, frame: shutdown(server, True))
+    # This will also catch KeyboardInterrupt exception
+    signal.signal(signal.SIGINT, lambda sig, frame: shutdown(server, False))
+    # Start the server
+    server.listen(options.port, "")
     IOLoop.instance().start()
 
 
 if __name__ == "__main__":
     main()
-
